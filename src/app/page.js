307135f3cffe1d210b10420350775975886b0aa6
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-10-15 11:21:19 -0300

    inital configs

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main className="min-h-[60vh] mb-20">
-      <h1>mini blog</h1>
+    <main >
+      <div>
+        <h1>mini blog</h1>
+      </div>
     </main>
   )
 }
